ef48ac2de46d76271e7340a11f2d418cf04359ac
arunravi180794@gmail.com <arunravi180794@gmail.com>
2021-09-23 13:59:23 +0530

    update

--- Bhavin/src/test/java/Demo1/ContactTest.java
+++ Bhavin/src/test/java/Demo1/ContactTest.java
@@ -6,7 +6,12 @@ public class ContactTest {
 	
 	@Test(groups="SmokeSuite")
 	public void createContactTest() {
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser");
 		System.out.println("execute createContactTest");
+		System.out.println("===="+url);
+		System.out.println("===="+browser);
+
 	}
 
 	@Test(groups="RegresionSuite")
